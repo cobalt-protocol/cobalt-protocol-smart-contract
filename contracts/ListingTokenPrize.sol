0e8631aff96b17693a62e7760e4c4d90144086d4
Eldovyn <farras.pramudita@gmail.com>
2026-09-21 01:50:07 +0700

    feat(contracts): add deactivateListingTokenPrize function

--- contracts/ListingTokenPrize.sol
+++ contracts/ListingTokenPrize.sol
@@ -17,7 +17,14 @@ contract ListingTokenPrizeContract is Ownable {
 
     event ListingTokenPrizeAdded(
         uint256 indexed listingTokenPrizeId,
-        address indexed tokenAddress
+        address indexed tokenAddress,
+        bool isActive
+    );
+
+    event ListingTokenPrizeDeactivated(
+        uint256 indexed listingTokenPrizeId,
+        address indexed tokenAddress,
+        bool isActive
     );
 
     constructor(address initialOwner) Ownable(initialOwner) {}
@@ -36,12 +43,32 @@ contract ListingTokenPrizeContract is Ownable {
             isActive: true
         });
 
-        emit ListingTokenPrizeAdded(listingTokenPrizeId, _tokenAddress);
+        emit ListingTokenPrizeAdded(listingTokenPrizeId, _tokenAddress, true);
     }
 
-    function isTokenListed(
+    function deactivateListingTokenPrize(
         address _tokenAddress
-    ) external view returns (bool) {
+    ) external onlyOwner {
+        require(
+            listingToken[_tokenAddress].listingTokenPrizeId != 0,
+            "Token is not listed"
+        );
+
+        require(
+            listingToken[_tokenAddress].isActive,
+            "Token already deactivated"
+        );
+
+        listingToken[_tokenAddress].isActive = false;
+
+        emit ListingTokenPrizeDeactivated(
+            listingToken[_tokenAddress].listingTokenPrizeId,
+            _tokenAddress,
+            listingToken[_tokenAddress].isActive
+        );
+    }
+
+    function isTokenListed(address _tokenAddress) external view returns (bool) {
         return listingToken[_tokenAddress].listingTokenPrizeId != 0;
     }
 }
